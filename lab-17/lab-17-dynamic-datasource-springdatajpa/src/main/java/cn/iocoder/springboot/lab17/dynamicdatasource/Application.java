43d13f6c9c663e8af2585eeacd0d3e042f0cade6
guoyongzheng <guoyongzheng@163.com>
2022-08-26 11:20:36 +0800

    1

--- lab-17/lab-17-dynamic-datasource-springdatajpa/src/main/java/cn/iocoder/springboot/lab17/dynamicdatasource/Application.java
+++ lab-17/lab-17-dynamic-datasource-springdatajpa/src/main/java/cn/iocoder/springboot/lab17/dynamicdatasource/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true) // http://www.voidcn.com/article/p-zddcuyii-bpt.html
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+// http://www.voidcn.com/article/p-zddcuyii-bpt.html
 public class Application {
 }
